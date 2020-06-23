e2ddcd9fe1c00572023801c4724605979bf7333b
遇见王斌 <meetbill@163.com>
2020-06-23 17:10:07 +0800

    [Changed]: update ruqi ，http ==> Request，增加 id 显示

--- examples/ruqi/src/static/js/controllers/ruqi/crontab.js
+++ examples/ruqi/src/static/js/controllers/ruqi/crontab.js
@@ -1,48 +1,55 @@
-﻿'use strict';
+'use strict';
 
 /* Controllers */
 
-//app.
-//    controller('CrontabCtrl', ['$scope', '$filter', '$http', function($scope, $filter, $http){
-app.controller('CrontabCtrl', ['$scope', '$filter', '$http', 'editableOptions', 'editableThemes',
-  function($scope, $filter, $http, editableOptions, editableThemes){
+app.controller('CrontabCtrl', ['$scope', '$filter', '$http', 'editableOptions', 'editableThemes', 'Request',
+function($scope, $filter, $http, editableOptions, editableThemes, Request) {
     editableThemes.bs3.inputClass = 'input-sm';
     editableThemes.bs3.buttonsClass = 'btn-sm';
     editableOptions.theme = 'bs3';
-    // editable table
     //$scope.jobs = [
     //  {name: 'exe_cmd', status: 2, cmd: "ls", rule: '* * * * * ',nexttime :"20190808 20:24"},
     //];
-    $http.get("ruqi/get_jobs").success(function (response) {$scope.jobs = response.data;})
+    $scope.update_jobs = function() {
+        Request.get('ruqi/get_jobs',
+        function(res) {
+            $scope.jobs = res.data;
+        },
+        false, true);
+    };
 
     // remove job
     $scope.removeJob = function(name) {
-        var remove_job = $http.post("ruqi/remove_job",{
-                "name":name}
-                )
-        remove_job.success(function (response) {
-            for (let i = $scope.jobs.length - 1; i >= 0 ; i--) {
-                if($scope.jobs[i].name === name) {
-                    console.log(i)
+        Request.post('ruqi/remove_job', {
+            'name': name
+        },
+        function(res) {
+            for (let i = $scope.jobs.length - 1; i >= 0; i--) {
+                if ($scope.jobs[i].name === name) {
                     $scope.jobs.splice(i, 1);
                 }
             };
-        })
+        });
     };
+
     // add job
     $scope.addJob = function() {
-      $scope.inserted = {
-        name: '',
-        status: null,
-        cmd: null,
-        rule: null,
-        nexttime: null,
-      };
-      $scope.jobs.push($scope.inserted);
+        $scope.inserted = {
+            name: '',
+            status: null,
+            cmd: null,
+            rule: null,
+            nexttime: null,
+        };
+        $scope.jobs.push($scope.inserted);
     };
     // update job
     $scope.saveJob = function(data) {
-      //$scope.user not updated yet
-      return $http.post('ruqi/add_job', data);
+        Request.post("ruqi/add_job", data,
+        function(res) {
+            $scope.update_jobs();
+        });
     };
-  }]);
+
+    $scope.update_jobs();
+}]);
